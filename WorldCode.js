6c5dc650ca104e29f502c3a03a62a274607c4660
timofeycacti <149524909+timofeycacti@users.noreply.github.com>
2025-08-20 10:28:42 +0200

    Impulse Cacti!
    Small update, added Cacti that send a signal every 25 ticks

--- WorldCode.js
+++ WorldCode.js
@@ -1,18 +1,20 @@
-//By cvetochekcactus on Github!
+//By timofeycacti in Github 
 let RBlocks=[];
 let lb=[] //litten up blocks
+let cacti=[] //"Cactuses" is wrong, according to grammar
+
 
 function bget(x,y,z){
   return api.getBlock(x,y,z);
 }
 
-
+mullis=0
 function tick(){
-if (lb.length>0){ // checking if there are bugged blocks
-	let [xx,yy,zz] = lb.shift(); //getting and deleting the first item
-	let matrix=[[-1,0],[0,-1],[0,1],[1,0]]	//creating matrix
+if (lb.length>0){
+	let [xx,yy,zz] = lb.shift();
+	let matrix=[[-1,0],[0,-1],[0,1],[1,0]]	
 	for (let b of matrix){
-    if (bget(xx+b[0],yy,zz+b[1]) == "Dirt") { //doing almost the same as in the Light function, but without lighting the near blocks up
+    if (bget(xx+b[0],yy,zz+b[1]) == "Dirt") {
       api.setBlock(xx+b[0],yy,zz+b[1],"Grass Block");
     } else if (bget(xx+b[0],yy,zz+b[1]) == "Grass Block") {
       api.setBlock(xx+b[0],yy,zz+b[1],"Dirt");
@@ -20,6 +22,15 @@ if (lb.length>0){ // checking if there are bugged blocks
 	}
 	api.setBlock(xx,yy,zz,"Maple Leaves")
 }
+
+if ((mullis%25)==0){ //Every 25 Ticks
+for (let block of cacti){
+Light(block[0],block[1],block[2]) //Light every Cactus block
+}
+}
+
+
+mullis++
 }
 
 function Light(x,y,z){
@@ -62,17 +73,25 @@ api.clearInventory(id)
 api.giveItem(id,"Maple Leaves",999,{customDisplayName:"Wire"})
 api.giveItem(id,"Maple Wood Planks",999,{customDisplayName:"Lever"})
 api.giveItem(id,"Dirt",999,{customDisplayName:"Lamp"})
+api.giveItem(id,"Fat Cactus",999,{customDisplayName:"Impulse Cactus"})
+
 api.sendMessage(id,"Welcome to the Leafstone Beta by cvetochekcactus!\n P.S. Activate the lever by RMBing it!")
 }
 
 function playerCommand(id,cmd){
 
-if (cmd == "bugged"){
+if (cmd == "bugged"){ // a debug command to see all "bugged" blocks
 api.log(lb)
 }
-
+api.log(cmd)
 }
 
-
+function onPlayerChangeBlock(id,x,y,z,from,to){
+if (to=="Fat Cactus"){
+cacti.push([x,y,z])
+} else if (from.startsWith("Fat Cactus")){
+cacti.pop(cacti.indexOf([x,y,z]))
+}
+}
 
 
